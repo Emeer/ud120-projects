16726eb1810789d26daf2a6a5416487a765aae6d
Jia ZHOU <jiazhou@egencia.com>
2018-09-19 17:14:57 +0200

    Done for SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,6 +10,10 @@
     
 import sys
 from time import time
+
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -19,11 +23,29 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+
+#clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+prd = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "Accuracy:", accuracy_score(labels_test, prd)
+
+#print "10th: %r, 26th: %r, 50th: %r" % (prd[10], prd[26], prd[50])
+
+print "No. of predicted to be in the 'Chris'(1): %r" % sum(prd)
 
 #########################################################
 
